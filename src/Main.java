a3c5cc3e180a143a340ff582c8fa538ac3253372
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-07-06 19:55:51 +0700

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,84 +7,34 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
+    public static Date date = new Date();
+    public static StringBuilder logger = new StringBuilder();
 
     public static void main(String[] args) {
-        Date date = new Date();
-        StringBuilder logger = new StringBuilder();
+        String logPath = "C:/Users/minim/STUDY/JD-45/Games/temp/temp.txt";
+        List<String> pathObjects = new ArrayList<>();
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/src");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/res");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/temp");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/src/main");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/src/test");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/src/main/Main.java");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/src/main/Utils.java");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/res/drawables");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/res/vectors");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/res/icons");
+        pathObjects.add("C:/Users/minim/STUDY/JD-45/Games/savegames");
+        pathObjects.add(logPath);
 
-        File dirSrc = new File("C:/Users/minim/STUDY/JD-45/Games/src");
-        if (dirSrc.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/src/\n");
-        }
-
-        File dirRes = new File("C:/Users/minim/STUDY/JD-45/Games/res");
-        if (dirRes.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/res/\n");
-        }
-
-        File dirSavegames = new File("C:/Users/minim/STUDY/JD-45/Games/savegames");
-        if (dirSavegames.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/savegames/\n");
-        }
-
-        File dirTemp = new File("C:/Users/minim/STUDY/JD-45/Games/temp");
-        if (dirTemp.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/temp/\n");
-        }
-
-        File dirMain = new File("C:/Users/minim/STUDY/JD-45/Games/src/main");
-        if (dirMain.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/src/main/\n");
-        }
-
-        File dirTest = new File("C:/Users/minim/STUDY/JD-45/Games/src/test");
-        if (dirTest.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/src/test/\n");
-        }
-
-        File fileMain = new File("C:/Users/minim/STUDY/JD-45/Games/src/main/", "Main.java");
-        try {
-            if (fileMain.createNewFile()) {
-                logger.append(date + " создан файл C:/Users/minim/STUDY/JD-45/Games/src/main/Main.java\n");
+        for (String file : pathObjects) {
+            if (file.contains(".")) {
+                createFile(file);
+            } else {
+                createDirectory(file);
             }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
         }
 
-        File fileUtils = new File("C:/Users/minim/STUDY/JD-45/Games/src/main/", "Utils.java");
-        try {
-            if (fileUtils.createNewFile()) {
-                logger.append(date + " создан файл C:/Users/minim/STUDY/JD-45/Games/src/main/Utils.java\n");
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        File dirDrawables = new File("C:/Users/minim/STUDY/JD-45/Games/res/drawables");
-        if (dirDrawables.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/res/drawables/\n");
-        }
-
-        File dirVectors = new File("C:/Users/minim/STUDY/JD-45/Games/res/vectors");
-        if (dirVectors.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/res/vectors/\n");
-        }
-
-        File dirIcons = new File("C:/Users/minim/STUDY/JD-45/Games/res/icons");
-        if (dirIcons.mkdir()) {
-            logger.append(date + " создана директория C:/Users/minim/STUDY/JD-45/Games/res/icons/\n");
-        }
-
-        File fileTemp = new File("C:/Users/minim/STUDY/JD-45/Games/temp", "temp.txt");
-        try {
-            if (fileTemp.createNewFile()) {
-                logger.append(date + " создан файл C:/Users/minim/STUDY/JD-45/Games/temp/temp.txt\n");
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try (FileWriter writer = new FileWriter("C:/Users/minim/STUDY/JD-45/Games/temp/temp.txt", true)) {
+        try (FileWriter writer = new FileWriter(logPath)) {
             writer.write(logger.toString());
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -109,6 +59,33 @@ public class Main {
                 "/zip_savegames.zip", "C:/Users/minim/STUDY/JD-45/Games/savegames/");
 
         System.out.println(openProgress("C:/Users/minim/STUDY/JD-45/Games/savegames/save3.dat"));
+    }
+
+    public static boolean createFile(String filePath) {
+        File file = new File(filePath);
+        try {
+            if (file.createNewFile()) {
+                logger.append(date + " create file " + filePath + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean createDirectory(String directoryPath) {
+        File dir = new File(directoryPath);
+        if (dir.mkdir()) {
+            logger.append(date + " create directory " + directoryPath + "\n");
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean writeThisFile(String filePath) {
+        return true;
     }
 
     public static boolean saveGame(String saveFilePath, GameProgress game) {
